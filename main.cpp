79afc2497bb9b5cc2a4b59364f238048a9ea09fc
IDeal <cyon256@icloud.com>
2025-09-25 06:47:57 +0900

    c++

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-int main ()
+
+int getValueFromUser()
 {
 	std::cout << "Enter an integer: ";
-	int num{};
-	std::cin >> num;
-	std::cout << "Double that number is: " << num * 2 << '\n';
+	int input{};
+	std::cin >> input;
+	return input;
+}
+
+void printValue( int x, int y)
+{
+	std::cout << x << '\n';
+	std::cout << y << '\n';
+}
+
+int main ()
+{
+	int x { getValueFromUser() };
+	int y { getValueFromUser() };
+	printValue(x, y);
 	return 0;
 }
